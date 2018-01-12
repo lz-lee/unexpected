0fbcb29bbc9869a38377c0ff6387f55677f494f4
lz-lee <296452017@qq.com>
2018-01-12 14:42:31 +0800

    add shuffle

--- util.js
+++ util.js
@@ -34,7 +34,7 @@ let vendor = (() => {
   return false
 })()
 
-export fucntion prefixStyle(style) {
+export function prefixStyle(style) {
   if (vendor === false) {
     return false
   }
@@ -47,4 +47,137 @@ export fucntion prefixStyle(style) {
   }
 
   return vendor + style.charAt(0).toUpperCase() + style.substring(1)
+}
+/**
+ * 数组洗牌
+ * @param {Array} arr
+ */
+export function shuffle(arr) {
+  let _arr = arr.slice()
+  for (let i = 0; i < _arr.length; i++) {
+    let j = getRandomIndex(0, i)
+    let t = _arr[i]
+    _arr[i] = _arr[j]
+    _arr[j] = t
+  }
+}
+
+/**
+ * 去抖函数
+ * @param {Function} fn
+ * @param {Number} delay
+ */
+export function debounce(fn, delay) {
+  let timer
+  return function(...args) {
+    if (timer) clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn.apply(this, args)
+    }, delay)
+  }
+}
+
+/**
+* 解析url 参数
+* @example ?id=12345&a=b
+* @return Object {id:12345,a:b}
+*/
+export function getParameter() {
+	let search = window.location.search.substr(1);
+	let vars = search.split('&');
+	let parameters = {};
+	vars.forEach((item) => {
+		let temp = item.split('=');
+		let key = decodeURIComponent(temp[0]);
+		let val = decodeURIComponent(temp[1]);
+		parameters[key] = val;
+	});
+	return parameters;
+}
+
+/**
+ * 拼接get请求参数
+ * data为键值对对象
+ * {id: 5, name: 'lee'}
+ * @return url?id=5&name=lee
+ */
+
+export function formatUrl(url, data) {
+	url += (url.indexOf('?') < 0 ? '?' : '&') + formatParams(data)
+	return url
+}
+
+function formatParams(data) {
+	let url = ''
+	for (let k in data) {
+		let value = data[k] !== undefined ? data[k] : ''
+		url += `&${k}=${encodeURIComponent(value)}`
+	}
+	return url.substring(1)
+}
+
+/**
+ *
+ * @param {Date} date Date对象，后端返回的时间戳乘1000
+ * @param {String} fmt 'yyyy-MM-dd hh:mm'
+ */
+export function formatDate(date, fmt) {
+	if (/(y+)/.test(fmt)) {
+		fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+	}
+	let o = {
+		'M+': date.getMonth() + 1,
+		'd+': date.getDate(),
+		'h+': date.getHours(),
+		'm+': date.getMinutes(),
+		's+': date.getSeconds()
+	};
+
+	for (let k in o) {
+		if (new RegExp(`${k}`).test(fmt)) {
+			let str = o[k] + '';
+			fmt = fmt.replace(new RegExp(`${k}`), (RegExp.$1.length === 1) ? str : padLeftZero(str));
+		}
+	}
+	return fmt;
+};
+
+/**
+ * 导出excel
+ * featch api不支持IE
+ */
+export function exportExcel(url, data) {
+  let token = 'token'
+
+  let headers = new Headers()
+
+  headers.append('token', token)
+
+  let url = formatUrl(url, data)
+
+  let request = new Request(url, {headers: headers, method: 'GET'})
+
+  fetch(request).then(res => res.blob().then(blob => {
+
+    let a = document.createElement('a')
+    let url = window.URL.createObjectURL(blob)
+    let filename = 'myfile.xls'
+    a.href = url
+    // a标签的download属性
+    a.download = filename
+
+    a.click()
+
+    window.URL.revokeObjectURL(url)
+  }))
+}
+
+
+function padLeftZero(str) {
+	return ('00' + str).substr(str.length)
+}
+
+
+function getRandomIndex(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
