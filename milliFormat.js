96df6630c596b512274d1ddf4b8b1a7d005b807d
李良知 <296452017@qq.com>
2019-04-23 10:56:16 +0800

    千位分隔

---
+++ milliFormat.js
@@ -0,0 +1,23 @@
+/**
+ * 千位分隔，可以处理负数、整数、小数
+ * @param {num} num
+ */
+export const milliFormat = num => {
+  return num && num.toString().replace(/-?\d+/, (m) => {
+    return m.replace(/(\d+)(?=(\d{3})+$)/g, '$1,')
+  })
+}
+
+// 或者使用 Number.prototype.toLocalString('en-US', options)
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
+
+/**
+ *
+ * @param {需要格式化的数字} num
+ * @param {最小小数位位数} scale
+ */
+export const formatValue = (num, scale) => {
+  return num && num.toLocalString('en-US', {
+    minimumFractionDigits: scale
+  })
+}
