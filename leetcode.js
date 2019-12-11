cfb7152bc145715a0fff320f45305ff73b807070
李良知 <296452017@qq.com>
2019-12-11 16:29:01 +0800

    feat: add leetcode

---
+++ leetcode.js
@@ -0,0 +1,30 @@
+
+/**
+ * 26.删除排序数组中的重复项
+ * 双指针
+ * @param [0,0,1,1,1,2,2,3,3,4]
+ * @return 5
+ */
+function removeDuplicates(arr) {
+    const len = nums.length;
+    let slow = 0;
+    for (let fast = 0; fast < len; fast++) {
+        // slow 和 fast 不想等时 slow 前进一位
+        if (nums[slow] !== nums[fast]) {
+            slow++;
+            nums[slow] = nums[fast]
+        }
+
+    }
+    return slow + 1;
+}
+
+/**
+ * 104.求二叉树深度
+ * 递归
+ */
+function maxDepth(root) {
+    if (!root) return 0;
+    if (!root.left && !root.right) return 1;
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+}
