923cd8cc899744e054a10b08a7d2c9aacfa83bdc
liliangzhi <296452017@qq.com>
2021-08-12 14:51:24 +0800

    fix deepClone

--- deep-clone.js
+++ deep-clone.js
@@ -3,10 +3,10 @@ function deepClone(obj, cache = new WeakMap()) {
         return obj;
     }
     // const hit = cache.find(v => v.original === obj);
-    const hit = cache.get(obj)
+    const hit = cache.get(obj);
     if (hit) {
         // return hit.copy;
-        return hit
+        return hit;
     }
     const copy = Array.isArray(obj) ? [] : {};
     // cache 处理循环引用的情况
@@ -16,24 +16,24 @@ function deepClone(obj, cache = new WeakMap()) {
     // });
     cache.set(obj, copy);
     // ownKeys = Object.getOwnPropertyNames （包括不可枚举属性） (而 Object.keys 返回的是自身可枚举属性组成的数组，“可枚举”即“可遍历”) + Object.getOwnPropertySymbols
-    Reflect.ownKeys(obj).forEach(key => {
+    // for...in语句以任意顺序遍历一个对象的除Symbol以外的可枚举属性。
+    Reflect.ownKeys(obj).forEach((key) => {
         copy[key] = deepClone(obj[key], cache);
     });
     return copy;
 }
 
-
 const test = {
-    a: [1,2, Symbol('123')],
+    a: [1, 2, Symbol('123')],
 
     b: {
         c: 45,
         d: Symbol('abc'),
     },
-    aa: null
-}
+    aa: null,
+};
 
-test.f = test
+test.f = test;
 
 const newTest = deepClone(test);
 console.log(newTest);
