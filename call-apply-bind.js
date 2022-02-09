fb447e5a60beedd1b22838cb82ccd2895de57876
liliangzhi <296452017@qq.com>
2022-02-09 12:11:59 +0800

    fix: bind

--- call-apply-bind.js
+++ call-apply-bind.js
@@ -25,24 +25,7 @@ Function.prototype.myApply = function (ctx, ...args) {
 };
 
 // impl bind
-// 方案一
-Function.prototype.bind = function (ctx, ...args) {
-    if (typeof this !== 'function') {
-        throw new Error('Function.prototype.bind - - what is trying to be bound is not callable');
-    }
-    const that = this;
-    function bound(...newArgs) {
-        // new 操作符使用 会执行 [[constructor]] 函数，new.target 指向构造函数
-        // this instanceof bound 也可以判断是否被 new 操作符使用
-        // instanceof 有缺陷，当被外部函数显示的 call 时 this 不一定指向 bound
-        return that.apply(new.target === bound ? this : ctx, [...args, ...newArgs]);
-    }
-    // 维护原型链 或者 通过一个空函数中转
-    bound.prototype = Object.create(this.prototype);
-    return bound;
-};
 
-// 方案二
 Function.prototype.myBind = function (context, ...args) {
     if (typeof this !== 'function') {
         throw new TypeError('Error');
@@ -62,9 +45,9 @@ Function.prototype.myBind = function (context, ...args) {
 function foo(...args) {
     console.log(args, '<<<args');
     this.args = args;
-    console.log(this.name, '<<this.name')
+    console.log(this.name, '<<this.name');
 }
 
-var ff = foo.bind({ name: 'AAA' }, 1, 2, 3);
+var ff = foo.myBind({ name: 'AAA' }, 1, 2, 3);
 
 console.log(new ff());
