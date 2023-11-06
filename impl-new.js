5c053ff26c47eb410072a45cccc37b81e4453175
liliangzhi <296452017@qq.com>
2023-11-06 21:52:07 +0800

    fix: new return null should return instance

--- impl-new.js
+++ impl-new.js
@@ -1,5 +1,6 @@
 const myNew = (fn, ...args) => {
-  const obj = Object.create(fn.prototype)
-  const result = fn.call(obj, ...args)
-  return typeof result === 'object' ? result : obj
-}
+  const obj = Object.create(fn.prototype);
+  const result = fn.call(obj, ...args);
+  return typeof result === 'object' && result !== null ? result : obj;
+  // result instanceof Object ? result : obj;
+};
