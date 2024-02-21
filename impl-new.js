25550e4283bc9c186b81007532eefd6b80752183
lz-lee <296452017@qq.com>
2024-02-21 20:34:21 +0800

    Update impl-new.js

--- impl-new.js
+++ impl-new.js
@@ -1,6 +1,5 @@
 const myNew = (fn, ...args) => {
   const obj = Object.create(fn.prototype);
   const result = fn.call(obj, ...args);
-  return typeof result === 'object' && result !== null ? result : obj;
-  // result instanceof Object ? result : obj;
+  return result instanceof Object ? result : obj;
 };
